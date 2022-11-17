f4f9c82252640c0b4af7f436f2272b7d04d0a780
Francisco Sevilla <fj.sevillamora@gmail.com>
2022-11-17 11:44:15 -0600

    Demo API update

--- AccountOwnerServer/Controllers/EmployeeController.cs
+++ AccountOwnerServer/Controllers/EmployeeController.cs
@@ -29,12 +29,12 @@ namespace AccountOwnerServer.Controllers
         {
             try
             {
-                var employees = await _repository.Employee.GetAllEmployeesAsync();
-                _logger.LogInfo("Returned all employees from the database");
+                // var employees = await _repository.Employee.GetAllEmployeesAsync();
+                // _logger.LogInfo("Returned all employees from the database");
 
-                var employeesResult = _mapper.Map<IEnumerable<EmployeeDto>>(employees);
-                return Ok(employeesResult);
-                // return Ok("Return all employees");
+                // var employeesResult = _mapper.Map<IEnumerable<EmployeeDto>>(employees);
+                // return Ok(employeesResult);
+                return Ok("Return all employees");
             }
             catch (Exception ex)
             {
